b462422b2b2c3217abdf73cb4ddc7abf9198c377
joshlinx69 <lyou09134@gmail.com>
2026-04-17 16:50:04 +0800

    A few more test to fix code runner button and c/c++ compiler

--- c_plus_plus.cpp
+++ c_plus_plus.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
+bool isInThere (vector<int>& vec, int n){
+    for (auto it = vec.begin(); it != vec.end(); it++){
+        if (*it == n){
+            return true;
+        }
+    }
+    return false;
+}
+
+void disVect(vector<int>& vec){
+    for (int i = 0; i < vec.size(); i++){
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
+    int a;
+    cin >> a;
+    vector<int> nums = {1, 3, 4, 16, 25, 57, 72};
+    if (isInThere(nums, a)){
+        cout << a << " is in the vector." << endl;
+    }
+    else {
+        cout << a << " is not in the vector." << endl;
+        nums.push_back(a);
+        cout << "The vector has been updated: ";
+        disVect(nums);
+    }
     return 0;
 }
