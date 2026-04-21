943eb91fa30a7c2420462f88519b181fac61a570
joshlinx69 <lyou09134@gmail.com>
2026-04-21 11:23:44 +0800

    Practiced C language

--- c_test.c
+++ c_test.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    int *ptr;
-    int a = 16;
+int addition (int a, int b);
+int subtraction (int a, int b);
+int product (int a, int b);
+int int_division (int a, int b);
+void print_array (int arr[], int n);
 
-    ptr = &a;
-    printf("%d", *ptr);
+int main() {
+    int *ptr, c;
+    int mat[3][3]= {{1, 0, 3},
+                    {0, 5, 5},
+                    {2, 1, 8}};
+    ptr = &mat[2][2];
+    printf("The value pointed by ptr is: %d\n", *ptr);
+    printf("%s\n", mat[0] == &(mat[0][0])? "True":"False");
     return 0;
+}
+
+int addition (int a, int b) {
+    return a+b;
+}
+
+int subtraction (int a, int b) {
+    return a-b;
+}
+
+int product (int a, int b) {
+    return a*b;
+}
+
+int int_division (int a, int b) {
+    if (b==0) {
+        printf("Error: Cannot divide by zero.\n");
+        return -1;
+    }
+    return a/b;
+}
+
+void print_1Darray (int arr[], int n) {
+    int i = 0;
+    while (i<n) {
+        printf("%d ", arr[i]);
+        i++;
+    }
+    printf("Finished\n");
 }
