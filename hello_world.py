7fa5e7b00a9f474b0aa3d5258bc2f15f4625ed05
林祖佑 <linzuyou@Joshs-MacBook-Air.local>
2026-04-30 20:33:39 +0800

    play with resizing image

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,17 @@
 import cv2
 
 try:
-    img = cv2.imread('C:\\Users\\joshlin69x69\\Pictures\\penny.jpg')
+    img = cv2.imread('/Users/linzuyou/Desktop/unicorn_gundam.png')
 except Exception as e:
     print(f"The image loading failed: {e}")
 
-cv2.imshow('Penny the Ginger Cat', img)
+scale_percent = 200
+width = int(img.shape[1]*scale_percent/100)
+height = int(img.shape[0]*scale_percent/100)
+
+resized = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+cv2.imwrite('/Users/linzuyou/Desktop/unicorn_gundam_resized.png', resized)
+
+cv2.imshow('Unicorn Gundam', resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
